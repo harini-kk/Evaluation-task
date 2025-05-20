d3f46b0e937e173a805c39c6df064f9a22368081
harini-kk <141908478+harini-kk@users.noreply.github.com>
2025-05-20 15:55:13 +0530

    Necessary fonts integrated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        helvetica: ["Helvetica", "Arial", "sans-serif"],
+        manrope: ["Manrope", "sans-serif"],
+      },
+      fontSize: {
+        "40px": "40px",
+      },
+      colors: {
+        primary: '#F95B5B',
+        secondary: '#0084D4',
+        gray: '#6D6D6D'
+      },
+    },
   },
   plugins: [],
 };
