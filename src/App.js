47abb255a313523be0e2932255fcc88b8661ad4c
harini-kk <141908478+harini-kk@users.noreply.github.com>
2025-05-20 16:09:14 +0530

    Announcement bar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import AnnouncementBar from './components/announcementBar';
 
 function App() {
   return (
     <div className="App">
-     Evaluation task
+     <AnnouncementBar/>
     </div>
   );
 }
